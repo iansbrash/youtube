0769a0c8b81fbe336e2011de98f410b37b99bc17
Ian Brash <brash@usc.edu>
2025-01-26 16:11:30 -0800

    feat: add missing files

---
+++ next-auth-role-based-access-control/prisma/client.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient | undefined;
+};
+
+// Helps us maintain only a single Prisma instance between all API endpoints
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({}); // .$extends(drizzle());
+
+if (process.env.NEXT_PUBLIC_ENVIRONMENT !== "prod")
+	globalForPrisma.prisma = prisma;
